278b922bfee8e5d307228873d0cb712810db1f08
Shorya Kaushik <shorya.kaushik@hashedin.com>
2021-05-05 23:22:24 +0530

    Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>App Created</p>
+    </div>
+  );
+};
+
+export default App;
